8a8c5fbd2399e7c663c81b34eb652f1b71b34442
atubak400 <atubak40@gmail.com>
2025-04-18 16:18:35 +0100

    Add EKS cluster setup with admin role and VPC module

---
+++ main.tf
@@ -0,0 +1,90 @@
+terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.1.0"
+
+  name = "eks-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b"]
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Project     = "EKS-CI-CD"
+    Environment = "dev"
+  }
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.8.4"
+
+  cluster_name    = "simple-eks"
+  cluster_version = "1.28"
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  enable_irsa = true
+
+  eks_managed_node_groups = {
+    on_demand = {
+      instance_types = ["t3.medium"]
+      min_size       = 1
+      max_size       = 2
+      desired_size   = 1
+    }
+
+    spot = {
+      capacity_type  = "SPOT"
+      instance_types = ["t3.medium", "t3a.medium"]
+      min_size       = 0
+      max_size       = 2
+      desired_size   = 1
+    }
+  }
+
+  access_entries = {
+    admin = {
+      principal_arn = "arn:aws:iam::136600023723:role/EKSAdminRole"
+      type          = "STANDARD"
+
+      policy_associations = {
+        cluster-admin = {
+          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+          access_scope = {
+            type = "cluster"
+          }
+        }
+      }
+    }
+  }
+
+  tags = {
+    Project     = "EKS-CI-CD"
+    Environment = "dev"
+  }
+}
+
+output "update_kubeconfig_command" {
+  value = "aws eks update-kubeconfig --name ${module.eks.cluster_name} --region us-east-1 --role-arn arn:aws:iam::136600023723:role/EKSAdminRole"
+}
